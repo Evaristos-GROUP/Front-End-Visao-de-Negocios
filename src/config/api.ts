2d3f9e706f3ca87ad0bc2ab694ae12a43fd38b7f
Helyson Santiago <82424411+helysonsantiago@users.noreply.github.com>
2025-06-29 23:33:41 -0300

    Update api.ts
    Signed-off-by: Helyson Santiago <82424411+helysonsantiago@users.noreply.github.com>

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_URL = "https://visaodenegocios.zyx/";
+export const BASE_URL = "https://www.visaodenegocios.xyz/";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL, 
